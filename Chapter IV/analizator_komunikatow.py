2e6a4311e7162095d8fa5aabefb7dd8857c4241c
PatrykD <patryk.dagiel@gmail.com>
2017-06-14 14:43:00 +0200

    Commit 14/06/2017 II

---
+++ Chapter IV/analizator_komunikatow.py
@@ -0,0 +1,10 @@
+message = input("Wprowadz komunikat: ")
+print("\nDlugosc Twojego Komunikatu: ", len(message))
+print("\nNajczesciej uzywana litera w jezyku polskim, 'a'")
+    if 'a' in message:
+        print("wystapila w Twoim komunikacie.")
+    else:
+        print("nie wystapila w Twoim komunikacie.")
+
+    input("\n\n Aby zakonczyc program, nacisnij klawisz enter")
+
---
+++ Chapter IV/licznik.py
@@ -0,0 +1,10 @@
+print("Liczenie: ")
+for i in range(10):
+    print(i, end=" ")
+
+print("\nLiczenie co piec:")
+for i in range(0, 50, 5):
+    print(i, end=" ")
+print("\nLiczenie od tylu:")
+for i in range(10, 0, -1):
+    print(i, end=" ")
