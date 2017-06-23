1912ad7d44b255a5045a9241d7f6db121ca8c21d
PatrykD <patryk.dagiel@gmail.com>
2017-06-23 14:45:45 +0200

    Commit 23/06/2017 I

---
+++ Chapter V/translator_slangu.py
@@ -0,0 +1,10 @@
+geek = {
+    "404" : "googlowanie; wyszukiwanie w internecie informacji dotyczacych jakiejs osoby.",
+    "googling" : "googlowanie, poszukiwanie odpowiedzi na internecie",
+    "keyboard plaque" : "(skojarzone z kamieniem nazebnym)zanieczyszczenia nagromadzone na klawiaturze komputera",
+    "Link Rot" : "Obumieranie linkow, proces w ktorym linki do stron www staja sie nieaktualne",
+    "Percussive maintenance" : "(konserwacja perkusyjna)naprawa urzadzenia elektronicznego przez stukniecie",
+    "uninstalled" : "Zwolniony z pracy, odinstalowany, popularne w okresie banki internetowej"
+}
+if "Dancing Baloney" in geek:
+    print("Wiem, co to jest Dancing Baloney.")
