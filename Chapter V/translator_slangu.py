115fd5b5650a8e49936b41eeff770552a43fef6b
PatrykD <patryk.dagiel@gmail.com>
2017-06-26 13:24:56 +0200

    Commit 26/06/2017 I

--- Chapter V/translator_slangu.py
+++ Chapter V/translator_slangu.py
@@ -8,3 +8,45 @@ geek = {
 }
 if "Dancing Baloney" in geek:
     print("Wiem, co to jest Dancing Baloney.")
+else:
+    print("Nie mam pojecia co to jest DB")
+print(geek.get("Dancing Baloney"))
+choice = None
+while choice != "0":
+    print(
+        """"
+        Translator slangu komputerowego
+        0 - zakoncz
+        1 - znajdz nowy termin
+        2 - dodaj nowy termin
+        3 - zmien definicje terminu
+        4 - usun termin
+        """
+    )
+    choice = input('Wybierasz:')
+    print()
+    if choice == "0":
+        print("Zegnaj")
+
+    elif choice == '1':
+        term = input("Jaki termin mam przetlumaczyc?")
+        if term in geek:
+            definition = geek[term]
+            print("\n", term, "oznacza", definition)
+        else:
+            print("Niestety, nie znam terminu")
+    elif choice == '2':
+        term = input("Jaki termin mam dodac?")
+        if term not in geek:
+            definition = input("\nPodaj definicje tego terminu")
+            geek[term] = definition
+            print(term, "zostal dodany")
+        else:
+            print("Termin juz istnieje, zmien jego definicje")
+    elif choice == '4':
+        term = input("Jaki termin mam usunac: ")
+        if term in geek:
+            del geek[term]
+            print("\nOk, usunieto", term)
+        else:
+            print("\nNie moge tego zrobic, terminu", term, "nie ma w slowniku")
